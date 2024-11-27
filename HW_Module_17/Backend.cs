fda18a8852baa5d66b9a5f3d842ac3963fcb5230
Вадим ИВТ-211 <rentsvadim@gmail.com>
2024-11-27 17:29:57 +0300

    Добавьте файлы проекта.

---
+++ HW_Module_17/Backend.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Module_17
+{
+    internal class Backend
+    {
+        // Абстрактный базовый класс для учетной записи
+        public abstract class Account
+        {
+            // Баланс учетной записи
+            public double Balance { get; set; }
+
+            // Процентная ставка
+            public double Interest { get; protected set; }
+
+            // Абстрактный метод для расчета процентной ставки
+            public abstract void CalculateInterest();
+        }
+
+        // Обычная учетная запись
+        public class RegularAccount : Account
+        {
+            public override void CalculateInterest()
+            {
+                // Основной расчет
+                Interest = Balance * 0.4;
+
+                // Коррекция процентной ставки
+                if (Balance < 1000)
+                {
+                    Interest = Balance * 0.2;
+                }
+                else
+                {
+                    Interest = Balance * 0.4;
+                }
+            }
+        }
+
+        // Зарплатная учетная запись
+        public class SalaryAccount : Account
+        {
+            public override void CalculateInterest()
+            {
+                Interest = Balance * 0.5;
+            }
+        }
+
+        // Калькулятор
+        public static class Calculator
+        {
+            public static void CalculateInterest(Account account)
+            {
+                account.CalculateInterest();
+            }
+        }
+
+    }
+}
---
+++ HW_Module_17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HW_Module_17.Backend;
+
+namespace HW_Module_17
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Создание обычного аккаунта
+            Account regularAccount = new RegularAccount { Balance = 2500 };
+            Calculator.CalculateInterest(regularAccount);
+            Console.WriteLine($"Процентная ставка для первого обычного аккаунта: {regularAccount.Interest}");
+
+            regularAccount = new RegularAccount { Balance = 500 };
+            Calculator.CalculateInterest(regularAccount);
+            Console.WriteLine($"Процентная ставка для второго обычного аккаунта: {regularAccount.Interest}");
+
+            // Создание зарплатного аккаунта
+            Account salaryAccount = new SalaryAccount { Balance = 4000 };
+            Calculator.CalculateInterest(salaryAccount);
+            Console.WriteLine($"Процентная ставка для зарплатного аккаунта: {salaryAccount.Interest}");
+        }
+    }
+}
